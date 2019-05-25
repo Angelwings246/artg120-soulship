5fd5123a4b85edde30eb14b0309187cee02a8245
unknown <aetan@ucsc.edu>
2019-05-24 20:28:50 -0700

    fixed the weird 7 bug

--- js/states/Settings.js
+++ js/states/Settings.js
@@ -105,52 +105,98 @@ Settings.prototype = {
 
     this.cursors = game.input.keyboard.createCursorKeys();
     game.input.keyboard.addCallbacks(this, null, null, this.capture);
+    this.capturing = false;
+    this.change_hotkey = null;
+    this.change_hotekey_text = "";
+
   },
   update: function(){
 
-    this.update_volume_text();
-    this.update_hotkey_text();
+    this.capture_special(); //the keyboard callback doesn't capture the arrow keys, which might be important
 
     if (game.input.keyboard.justPressed(Phaser.KeyCode.ESC)){
-      game.input.keyboard.removeCallbacks(); //clear the key capturing
+      game.input.keyboard.removeCallbacks(); //clear the key capturing callbacks
       game.state.start('MainMenu', true, false, this.main, this.alt, this.music_vol, this.sfx_vol);
-
     }
   },
   update_volume_text: function() {
     this.music_vol_text.text = "" + (10 * this.music_vol);
     this.sfx_vol_text.text = "" + (10 * this.sfx_vol);
   },
-  update_hotkey_text: function() {},
+  update_hotkey_text: function(hotkey_text, keybind) {
+    hotkey_text.text = keybind;
+  },
   vol_change: function(button, pointer, isOver) {
     switch (button){
       case this.music_down:
           if(this.music_vol >= 0.1) this.music_vol -= 0.1;
-          this.music_vol = Math.floor(this.music_vol * 10)/10;
+          this.music_vol = Math.round(10*this.music_vol)/10;
           break;
       case this.music_up:
           if(this.music_vol <= 0.9) this.music_vol += 0.1;
-          this.music_vol = Math.floor(this.music_vol * 10)/10;
+          this.music_vol = Math.round(10*this.music_vol)/10;
           break;
       case this.sfx_down:
           if(this.sfx_vol >= 0.1) this.sfx_vol -= 0.1;
-          this.sfx_vol = Math.floor(this.sfx_vol * 10)/10;          
+          this.sfx_vol = Math.round(10*this.sfx_vol)/10;          
           break;
       case this.sfx_up:
           if(this.sfx_vol <= 0.9) this.sfx_vol += 0.1;
-          this.sfx_vol = Math.floor(this.sfx_vol * 10)/10;
+          this.sfx_vol = Math.round(10*this.sfx_vol)/10;
           break;                  
       default:
         break;
-    }
+      }
+    console.log(this.music_vol + " " + this.sfx_vol);
+    this.update_volume_text();
 
   },
   begin_capture: function(button, pointer, isOver) {
+    var hotkey;
+    if(!this.capturing) {
+      switch (button){
+        case this.main_up:
+          this.change_hotkey = this.main.up;
+          this.change_hotekey_text = this.main_up_text;
+          this.update_hotkey_text(this.main_up_text, "");
+          break;
+        case this.main_down:
+          break;
+        case this.main_left:
+          break;
+        case this.main_right:
+          break;  
+        case this.alt_up:
+          break;
+        case this.alt_down:
+          break;
+        case this.alt_left:
+          break;
+        case this.alt_right:
+          break;                  
+        default:
+          break;
+      }
+    this.capturing = true;
 
+    }
   },
   capture: function(string, event) {
-    console.log(string);
-    console.log(event);
+    if(this.capturing) {
+      console.log(string);
+      console.log(event);
+      this.end_capture(this.change_hotkey, this.change_hotekey_text, keycode, string);
+    }
+  },
+  capture_special: function() {
+    if(this.capturing) {
+      this.end_capture(this.change_hotkey, this.change_hotekey_text, keycode, string);
+    }
+  },
+  end_capture: function(hotkey, text, keycode, string) {
+    this.capturing = false;
+    hotkey = keycode;
+    this.update_hotkey_text(text, string);
   },
   reset_settings: function() {
       this.main = {
