1905a933fc29c4ae901a900bd2a7e35edc72443c
Hunter Lynch <hunterlynch123@gmail.com>
2019-05-29 09:25:17 -0700

    adding early cutscene functionality

---
+++ js/states/Cutscene2.js
@@ -0,0 +1,14 @@
+// Implements second cutscene in game
+
+'use strict'
+
+var Cutscene2 = function(game){};
+Cutscene2.prototype = {
+	init: function(main, alt){
+	this.main = main;
+	this.alt = alt;
+},
+
+	preload: function(){
+		
+	},
