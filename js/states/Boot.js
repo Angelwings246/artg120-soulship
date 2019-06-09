6ac8b4af04640dca3f6fcc204e2b26622de9f4af
unknown <aetan@ucsc.edu>
2019-06-09 11:35:57 -0700

    fixed buttons scrolling the browser

--- js/states/Boot.js
+++ js/states/Boot.js
@@ -8,6 +8,8 @@ Boot.prototype = {
 		game.load.image('loading', 'assets/img/hp red.png'); //using the hp bar red bar as a temp load screen
 	},
 	create: function(){
+    game.input.keyboard.addKeyCapture([Phaser.KeyCode.SPACEBAR, Phaser.KeyCode.UP, 
+                        Phaser.KeyCode.DOWN, Phaser.KeyCode.LEFT, Phaser.KeyCode.RIGHT]); //stop scrolling on spacebar/arrows
 		game.state.start('Load');
 	}
 };
